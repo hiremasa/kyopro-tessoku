7df6c1ee21d8bd705950a5c8e1c4dae9d967b6af
hiremasa <syorikunn0901@gmail.com>
2022-12-09 23:58:18 +0900

    1D累積和

---
+++ 6.py
@@ -0,0 +1,10 @@
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
+import itertools
+A_acc = [0] + list(itertools.accumulate(A))
+
+for _ in range(Q):
+    l, r = map(int, input().split())
+    l -= 1
+    r -= 1
+    print(A_acc[r] - A_acc[l-1])
